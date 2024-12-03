e457b23227c647a759847949cf2bbd7dd556983f
Daniyor <daniyor.b@userdata.tech>
2024-12-04 00:23:21 +0800

    add authentication all

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -16,21 +16,21 @@ def get_blogs(db: Session = Depends(database.get_db) , current_user: schemas.Use
     return get_all_blogs(db)
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_blog(blog:schemas.Blog, db: Session = Depends(database.get_db)):
+def create_blog(blog:schemas.Blog, db: Session = Depends(database.get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
   return create(blog, db)
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_blog(id: int, db: Session = Depends(database.get_db)):
+def delete_blog(id: int, db: Session = Depends(database.get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return delete(id, db)
 
 @router.put("/{id}")
-def update_blog(id: int, blog: schemas.Blog, db: Session = Depends(database.get_db)):
+def update_blog(id: int, blog: schemas.Blog, db: Session = Depends(database.get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
   return update(id, blog, db)
 
 
 
 @router.get("/{id}", status_code=status.HTTP_201_CREATED, response_model=schemas.showBlog)
-def get_blog(id: int, db: Session = Depends(database.get_db)):
+def get_blog(id: int, db: Session = Depends(database.get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
   blog = db.query(models.Blog).filter(models.Blog.id == id).first()
   if not blog:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
